aaaf8b870e79989f42aa1c2572f40df5c544edf1
Vincent Heuken <me@vincentheuken.com>
2015-06-18 22:41:57 -0700

    logs connections and disconnects

--- project.clj
+++ project.clj
@@ -5,7 +5,8 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/clojurescript "0.0-2755"]]
 
-  :node-dependencies [[source-map-support "0.2.8"]]
+  :node-dependencies [[source-map-support "0.2.8"]
+                      [socket.io "1.3.5"]]
 
   :plugins [[lein-cljsbuild "1.0.4"]
             [lein-npm "0.4.0"]]
--- src/moomoo/core.cljs
+++ src/moomoo/core.cljs
@@ -3,7 +3,21 @@
 
 (nodejs/enable-util-print!)
 
+(def http (nodejs/require "http"))
+(def socketio (nodejs/require "socket.io"))
+
+(defn disconnect []
+  (.log js/console "A user has disconnected!"))
+
+(defn connection [socket]
+  (.log js/console "A user has connected!")
+  (.on socket "disconnect" disconnect))
+
 (defn -main []
-  (println "Hello world!"))
+  (println "Hello world!")
+  (let [app (.createServer http)
+        io (.listen socketio app)]
+    (.on io "connection" connection)
+    (.listen app 3001)))
 
 (set! *main-cli-fn* -main)
