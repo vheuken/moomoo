f50a1d713648dd8670d9ff2793fdc1ef7f904f16
Vincent Heuken <me@vincentheuken.com>
2015-06-22 02:47:20 -0700

    implemented get-all-users

--- src/moomoo/rooms.cljs
+++ src/moomoo/rooms.cljs
@@ -5,7 +5,17 @@
 (def redis-client (.createClient (nodejs/require "redis")))
 
 (defn set-username [room id username]
-  (.hset redis-client (string/join room ":users") id username))
+  (.hset redis-client (string/join [room ":users"])  id username))
 
 (defn get-username [room id callback]
-  (.hget redis-client (string/join room ":users") id callback))
+  (.hget redis-client (string/join [room ":users"]) id callback))
+
+(defn get-all-users [room callback]
+  (letfn [(users-to-list [err reply]
+            (let [user-hash (js->clj reply)]
+              (vec (map #(val %) user-hash))))
+          (c [err reply]
+            (callback err (users-to-list err reply)))]
+  (.hgetall redis-client (string/join [room ":users"])
+    (fn [err reply callback]
+      (c err reply)))))
--- test/moomoo/rooms-test.cljs
+++ test/moomoo/rooms-test.cljs
@@ -1,16 +1,37 @@
 (ns moomoo.rooms-test
   (:require-macros [cemerick.cljs.test
                     :refer (is deftest with-test run-tests testing test-var
-                            done)])
+                            done use-fixtures)])
   (:require [cemerick.cljs.test :as t]
+            [cljs.nodejs :as nodejs]
             [moomoo.rooms :as rooms]))
 
+(def redis-client (.createClient (nodejs/require "redis")))
+
+(defn clear-redis-fixture []
+  (.flushall redis-client))
+
+(use-fixtures :once clear-redis-fixture)
+
 (deftest ^:async username
-  (let [room "room:foo"
-        id "test"
+  (let [room "room:test1"
+        id   "test"
         user "Ricky"]
     (rooms/set-username room id user)
     (rooms/get-username room id
       (fn [err reply]
         (is (= user (.toString reply)))
+        (done)))))
+
+(deftest ^:async get-all-users
+  (let [room "room:test2"
+        user-id1 "One"
+        user-id2 "Two"
+        user1 "Paul"
+        user2 "Lee"]
+    (rooms/set-username room user-id1 user1)
+    (rooms/set-username room user-id2 user2)
+    (rooms/get-all-users room
+      (fn [err reply]
+        (is (= [user1 user2] reply))
         (done)))))
