48a23a38783bcacbb501a1eac51500a60ed848e4
Vincent Heuken <me@vincentheuken.com>
2015-06-23 01:02:58 -0700

    emits userslist on join and disconnect

--- src/moomoo/core.cljs
+++ src/moomoo/core.cljs
@@ -13,7 +13,10 @@
 (def app (.createServer http))
 (def io (.listen socketio app))
 
-
+(defn emit-userslistto-room [room]
+  (rooms/get-all-users room
+    (fn [err reply]
+      (.emit (.to io room) "userslist" (clj->js reply)))))
 
 (defn connection [socket]
   (println "A user has connected!")
@@ -21,14 +24,15 @@
     (.get rooms/redis-client (string/join ["users:" (.-id socket)])
       (fn [err reply]
         (if (not (nil? reply))
-          (let [room (.toString reply)]
-            (rooms/get-all-users room
-              (fn [err reply]
-                (.emit (.to io room) "userslist" (clj->js reply))))))))
+          (let [room (.toString reply)
+                id   (.-id socket)]
+            (.hdel rooms/redis-client (string/join [room ":users"] id))
+            (emit-userslistto-room room)))))
     (println "A user has disconnected!")))
   (.on socket "set_username"
     (fn [room username]
       (rooms/set-username room (.-id socket) username)
+      (emit-userslistto-room room)
       (println (string/join [username " has joined " room]))))
   (.on socket "join_room"
     (fn [room]
