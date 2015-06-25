18a1bcb82ed46d23a8b759651aab3abf246bb115
Vincent Heuken <me@vincentheuken.com>
2015-06-25 15:46:40 -0700

    pass roomid into template

--- src/server/core.cljs
+++ src/server/core.cljs
@@ -63,7 +63,7 @@
 (.use app (.static express "public"))
 (.get app "/" #(. %2 (sendFile "public/index.html")))
 
-(.get app "/rooms/:id" #(. %2 (render "room")))
+(.get app "/rooms/:id" #(. %2 (render "room" (clj->js {:roomid (.-id (.-params %1))}))))
 
 (defn -main []
   (println (string/join ["Listening on port " port]))
