d87f79ffb90cc101460e5dacd7d7ba16691f1cda
Vincent Heuken <me@vincentheuken.com>
2015-06-25 16:22:11 -0700

    defined room-id in frontend.core

--- src/frontend/core.cljs
+++ src/frontend/core.cljs
@@ -1,2 +1,6 @@
-(ns moomoo-frontend.core)
-(js/alert "CLJS!")
+(ns moomoo-frontend.core
+  (:require [om.core :as om :include-macros true]
+            [om.dom  :as dom :include-macros true]))
+
+(defonce room-id (.getAttribute (. js/document (getElementById "roomid")) "data"))
+(js/alert room-id)
