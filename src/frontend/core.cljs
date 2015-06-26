a411796c9eeaec2ec72550f6b857e56aaa826714
Vincent Heuken <me@vincentheuken.com>
2015-06-26 12:45:55 -0700

    can now set username

--- src/frontend/core.cljs
+++ src/frontend/core.cljs
@@ -3,4 +3,20 @@
             [om.dom  :as dom :include-macros true]))
 
 (defonce room-id (.getAttribute (. js/document (getElementById "roomid")) "data"))
-(js/alert room-id)
+(defonce room (str "room:" room-id))
+(defonce socket (js/io "http://localhost:3001"))
+(defonce app-state (atom {}))
+
+(defn users-list-view [users owner]
+  (reify
+    om/IRender
+    (render [this]
+      (dom/h1 nil "Users"))))
+
+(om/root users-list-view app-state {:target (. js/document (getElementById "userslist"))})
+
+(.on socket "connect" #(.emit socket "join_room" room))
+(.submit (js/$ "#username_form")
+  (fn []
+    (.emit socket "set_username" room (.val (js/$ "#username")))
+    false))
