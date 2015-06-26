0404b51c2c512a41bb72c4d82d47eb68fcac7093
Vincent Heuken <me@vincentheuken.com>
2015-06-26 13:17:03 -0700

    updates userlist

--- src/frontend/core.cljs
+++ src/frontend/core.cljs
@@ -5,18 +5,32 @@
 (defonce room-id (.getAttribute (. js/document (getElementById "roomid")) "data"))
 (defonce room (str "room:" room-id))
 (defonce socket (js/io "http://localhost:3001"))
-(defonce app-state (atom {}))
+(defonce app-state (atom {:users []}))
 
-(defn users-list-view [users owner]
+(defn user-view [user owner]
   (reify
     om/IRender
     (render [this]
-      (dom/h1 nil "Users"))))
+      (dom/li nil user))))
+
+(defn users-list-view [data owner]
+  (reify
+    om/IRender
+    (render [this]
+      (dom/div nil
+        (dom/h1 nil "Users")
+        (apply dom/h2 nil
+          (om/build-all user-view (:users data)))))))
 
 (om/root users-list-view app-state {:target (. js/document (getElementById "userslist"))})
 
 (.on socket "connect" #(.emit socket "join_room" room))
+
 (.submit (js/$ "#username_form")
   (fn []
     (.emit socket "set_username" room (.val (js/$ "#username")))
     false))
+
+(.on socket "userslist"
+  (fn [users]
+    (swap! app-state assoc :users users)))
