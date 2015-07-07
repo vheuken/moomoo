7dcb267f812516a7ca1baf0caa45fa2147fbcf3d
Vincent Heuken <me@vincentheuken.com>
2015-07-06 23:13:55 -0700

    moved rendering code to renderer

--- src/frontend/core.cljs
+++ src/frontend/core.cljs
@@ -1,6 +1,4 @@
-(ns moomoo-frontend.core
-  (:require [om.core :as om :include-macros true]
-            [om.dom  :as dom :include-macros true]))
+(ns moomoo-frontend.core)
 
 (defonce room-id (.getAttribute (. js/document (getElementById "roomid")) "data"))
 (defonce room (str "room:" room-id))
@@ -14,52 +12,6 @@
                           :message-received false}))
 
 (enable-console-print!)
-
-(defn user-view [user owner]
-  (reify
-    om/IRender
-    (render [this]
-      (dom/li nil user))))
-
-(defn users-list-view [data owner]
-  (reify
-    om/IRender
-    (render [this]
-      (dom/div nil
-        (dom/h3 nil "Users")
-        (apply dom/div nil
-          (om/build-all user-view (:users data)))))))
-
-(defn message-view [message owner]
-  (reify
-    om/IRender
-    (render [this]
-      (dom/li nil message))))
-
-(defn messages-view [data owner]
-  (reify
-    om/IRender
-    (render [this]
-      (apply dom/span nil
-        (om/build-all message-view (:messages data))))
-    om/IDidUpdate
-      (did-update [_ _ _]
-        (if (:message-received @app-state)
-          (let [div (js/$ "#messages")]
-            (.scrollTop div (.prop div "scrollHeight"))
-            (swap! app-state assoc :message-received false))))))
-
-(defn file-upload-progress-view [data owner]
-  (reify
-    om/IRender
-    (render [this]
-      (if (nil? (:upload-progress @app-state))
-        (dom/span nil "")
-        (dom/span nil (str (:upload-progress data) "%"))))))
-
-(om/root users-list-view app-state {:target (. js/document (getElementById "userslist"))})
-(om/root messages-view app-state {:target (. js/document (getElementById "messages"))})
-(om/root file-upload-progress-view app-state {:target (. js/document (getElementById "progress"))})
 
 (.on socket "connect" #(.emit socket "join_room" room))
 
---
+++ src/frontend/renderer.cljs
@@ -0,0 +1,51 @@
+(ns moomoo-frontend.renderer
+  (:require [om.core :as om  :include-macros true]
+            [om.dom  :as dom :include-macros true]
+            [moomoo-frontend.core :as core]))
+
+(defn user-view [user owner]
+  (reify
+    om/IRender
+    (render [this]
+      (dom/li nil user))))
+
+(defn users-list-view [data owner]
+  (reify
+    om/IRender
+    (render [this]
+      (dom/div nil
+        (dom/h3 nil "Users")
+        (apply dom/div nil
+          (om/build-all user-view (:users data)))))))
+
+(defn message-view [message owner]
+  (reify
+    om/IRender
+    (render [this]
+      (dom/li nil message))))
+
+(defn messages-view [data owner]
+  (reify
+    om/IRender
+    (render [this]
+      (apply dom/span nil
+        (om/build-all message-view (:messages data))))
+    om/IDidUpdate
+      (did-update [_ _ _]
+        (if (:message-received @core/app-state)
+          (let [div (js/$ "#messages")]
+            (.scrollTop div (.prop div "scrollHeight"))
+            (swap! core/app-state assoc :message-received false))))))
+
+(defn file-upload-progress-view [data owner]
+  (reify
+    om/IRender
+    (render [this]
+      (if (nil? (:upload-progress @core/app-state))
+        (dom/span nil "")
+        (dom/span nil (str (:upload-progress data) "%"))))))
+
+(om/root users-list-view core/app-state {:target (. js/document (getElementById "userslist"))})
+(om/root messages-view core/app-state {:target (. js/document (getElementById "messages"))})
+(om/root file-upload-progress-view core/app-state {:target (. js/document (getElementById "progress"))})
+
