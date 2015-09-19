1d902e298afd4bfce7a576afb3b5f429f487bb56
Vincent Heuken <me@vincentheuken.com>
2015-09-18 22:38:46 -0700

    added core/async as dependency

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
 
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/clojurescript "1.7.58"]
+                 [org.clojure/core.async "0.1.338.0-5c5012-alpha"]
                  [com.cemerick/clojurescript.test "0.3.3"]
                  [org.omcljs/om "0.9.0"]
                  [com.cognitect/transit-cljs "0.8.225"]]
