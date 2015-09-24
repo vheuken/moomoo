a14ac793b3faa6717c60935e2f03a9540b855e05
Vincent Heuken <me@vincentheuken.com>
2015-09-24 00:46:32 -0700

    change track is now atomic

--- redis/change_track.lua
+++ redis/change_track.lua
@@ -1,8 +1,24 @@
 local room_id = ARGV[1]
-local track = redis.call('lrange', 'room:' .. room_id .. ':change-track-list', -1, -1)[1]
+local track_num = ARGV[2]
+local sound_id = ARGV[3]
 
-if track ~= nil then
-  redis.call('del', 'room:' .. room_id .. ':change-track-list')
+redis.call('set', 'room:' .. room_id .. ':started?', 'false')
+
+redis.call('del', 'room:' .. room_id .. ':sync-start')
+
+local num_tracks = redis.call('hlen', 'room:' .. room_id .. ':music-info')
+
+if (tonumber(track_num) < 0) or
+   (tonumber(track_num) >= tonumber(num_tracks)) then
+  return nil
 end
 
-return track
+redis.call('del', 'room:' .. room_id .. ':track-complete')
+
+local track_id = redis.call('hget', 'room:' .. room_id .. ':track-order', track_num)
+
+redis.call('set', 'room:' .. room_id .. ':current-track', track_num)
+
+redis.call('set', 'room:' .. room_id .. ':current-sound', sound_id)
+
+return track_id
--- src/server/client-interface.cljs
+++ src/server/client-interface.cljs
@@ -32,6 +32,18 @@
 (defn change-track [room track-num sound-id]
   (println "Chainging track in room " room
            "to" track-num "with sound-id" sound-id)
+  (.eval rooms/redis-client "local room_id = ARGV[1] local track_num = ARGV[2] local sound_id = ARGV[3] redis.call('set', 'room:' .. room_id .. ':started?', 'false') redis.call('del', 'room:' .. room_id .. ':sync-start') local num_tracks = redis.call('hlen', 'room:' .. room_id .. ':music-info') if (tonumber(track_num) < 0) or (tonumber(track_num) >= tonumber(num_tracks)) then return nil end redis.call('del', 'room:' .. room_id .. ':track-complete') local track_id = redis.call('hget', 'room:' .. room_id .. ':track-order', track_num) redis.call('set', 'room:' .. room_id .. ':current-track', track_num) redis.call('set', 'room:' .. room_id .. ':current-sound', sound_id) return track_id"
+    0 room track-num sound-id
+    (fn [err reply]
+      (println reply)
+      (if-not (nil? reply)
+        (rooms/set-current-track-position room 0
+          (fn []
+            (.emit (.to io room)
+                   "track-change"
+                    reply
+                    sound-id)))))))
+  (comment
   (rooms/add-to-change-track-list room track-num
     (fn []
       (rooms/start-changing-track room
