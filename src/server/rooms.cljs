7102358b1ec8accd95cab2eb7a84e79fa3825b3c
Vincent Heuken <me@vincentheuken.com>
2015-10-02 20:04:24 -0700

    changed change-track to use redis-lua

--- src/server/rooms.cljs
+++ src/server/rooms.cljs
@@ -416,7 +416,7 @@
 (defn change-track [room track-num sound-id callback]
   (println "Chainging track in room " room
            "to" track-num "with sound-id" sound-id)
-  (.eval redis-client "local room_id = ARGV[1] local track_num = ARGV[2] local sound_id = ARGV[3] redis.call('set', 'room:' .. room_id .. ':started?', 'false') redis.call('del', 'room:' .. room_id .. ':sync-start') local num_tracks = redis.call('hlen', 'room:' .. room_id .. ':music-info') if (tonumber(track_num) < 0) or (tonumber(track_num) >= tonumber(num_tracks)) then return nil end redis.call('del', 'room:' .. room_id .. ':track-complete') local track_id = redis.call('hget', 'room:' .. room_id .. ':track-order', track_num) redis.call('set', 'room:' .. room_id .. ':current-track', track_num) redis.call('set', 'room:' .. room_id .. ':current-sound', sound_id) redis.call('set', 'room:' .. room_id .. ':waiting-to-start?', 'true') return track_id "
+  ((.scriptWrap redis-lua "changeTrack")
     0 room track-num sound-id
     (fn [err reply]
       (if-not (nil? reply)
