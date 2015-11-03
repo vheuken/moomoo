5c92e8c8269981b589b2faf610664d0a9ca76cc3
Vincent Heuken <me@vincentheuken.com>
2015-11-02 21:33:38 -0800

    fixed crash

--- redis/get_all_users.lua
+++ redis/get_all_users.lua
@@ -8,7 +8,7 @@ for i=1, #users_socket_ids - 1, 2 do
   local id = users_socket_ids[i]
   users_muted[users_muted_iter] = id
   users_muted[users_muted_iter+1] = redis.call('get', 'users:' .. id .. ':muted?')
-  users_muted_iter = users_muted_iter + 1
+  users_muted_iter = users_muted_iter + 2
 end
 
 return {users_socket_ids, users_muted}
