cc0c4538f406e0f07596844647632f5896a06183
Vincent Heuken <me@vincentheuken.com>
2016-01-05 01:05:52 -0800

    fixed issue in uploads

--- src/frontend/uploads.cljs
+++ src/frontend/uploads.cljs
@@ -140,7 +140,7 @@
                                        (let [first-inactive-upload-id (first (filter #(unpaused-and-not-started? (uploads %)) uploads-order))
                                              first-inactive-upload    (uploads first-inactive-upload-id)]
                                          (if (and (not (nil? first-inactive-upload))
-                                                  true #_(> (count (active-uploads uploads-order uploads))
+                                                  (> (count (active-uploads uploads-order uploads))
                                                      upload-slots))
                                            (swap! app-state/app-state
                                                   assoc
