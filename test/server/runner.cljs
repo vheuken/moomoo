b0f567f42b82693aa6d795d4843f61a197a52587
Vincent Heuken <me@vincentheuken.com>
2016-01-29 03:39:59 -0800

    removed the redis flushall stuff

--- test/server/runner.cljs
+++ test/server/runner.cljs
@@ -3,17 +3,8 @@
               [cljs.nodejs :as node]
               [moomoo.user-test]
               [moomoo.room-test :as room]
-              [moomoo.server-interface-test]
-              [moomoo.cleanup-tests]))
-
-(defonce redis-client (.createClient (node/require "redis")))
+              [moomoo.server-interface-test]))
 
 (doo-tests 'moomoo.user-test
            'moomoo.room-test
-           'moomoo.server-interface-test
-           'moomoo.cleanup-tests)
-
-
-(println "Flushing Redis...")
-(.flushall redis-client)
-
+           'moomoo.server-interface-test)
