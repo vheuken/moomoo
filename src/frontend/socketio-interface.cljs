ddb341a9ca533fd20587654f2336a5c301892868
Vincent Heuken <me@vincentheuken.com>
2016-01-30 08:36:54 -0800

    removed unnecessary print

--- src/frontend/socketio-interface.cljs
+++ src/frontend/socketio-interface.cljs
@@ -4,8 +4,6 @@
 
 (.on g/socket "sign-in-success"
   (fn [user-id users]
-    (println (.-username ((js->clj users) user-id)))
     (let [users (js->clj users)
           users (update-in users (keys users) #(js->clj %1))]
-      (println users)
       (client/sign-in-success! g/app-state user-id users))))
