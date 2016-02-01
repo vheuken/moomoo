005364fcbd11649347f1b3d10155e094d5bc0663
Vincent Heuken <me@vincentheuken.com>
2016-01-31 20:16:30 -0800

    reimplemented pause/stop/start/resume

--- src/frontend/uploads.cljs
+++ src/frontend/uploads.cljs
@@ -1,2 +1,23 @@
 (ns moomoo-frontend.uploads)
 
+(defonce  blank-upload {:stopped? true
+                        :paused?  false
+                        :id       nil
+                        :filename nil})
+
+(defn start [upload]
+  (assoc upload :stopped? false))
+
+(defn stop [upload]
+  (assoc upload :stopped? true))
+
+(defn pause [upload]
+  (assoc upload :paused? true))
+
+(defn resume [upload]
+  (assoc upload :paused? false))
+
+(defn get-action [old-state new-state upload-id]
+  "returns the action applied to the given upload-id.
+   Can return: :stopped, :started, :paused, :unpaused. :reconnected"
+  )
--- test/frontend/uploads-test.cljs
+++ test/frontend/uploads-test.cljs
@@ -1,3 +1,26 @@
 (ns moomoo-frontend.uploads-test
   (:require [cljs.test :refer-macros [deftest is testing]]
             [moomoo-frontend.uploads :as uploads]))
+
+(deftest start-upload
+  (is (= false (:stopped? (uploads/start uploads/blank-upload)))))
+
+(deftest stop-upload
+  (is (= true
+         (:stopped? (uploads/stop (uploads/start uploads/blank-upload))))))
+
+(deftest pause-upload
+  (is (= true (:paused? (uploads/pause uploads/blank-upload)))))
+
+(deftest pause-upload
+  (is (= false (:paused? (uploads/resume (uploads/pause uploads/blank-upload))))))
+
+(deftest get-action-started
+  (let [filename "foo.mp3"
+        upload-id "test-id"
+        upload (merge uploads/blank-upload {:filename filename
+                                            :id upload-id})
+        old-state {:uploads {}}
+        new-state {:uploads {upload-id
+                             (uploads/start upload)}}]
+    (uploads/get-action old-state new-state upload-id)))
