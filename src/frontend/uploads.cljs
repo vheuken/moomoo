c77f8547ebdc63cdc4945965e9ae91242f097e17
Vincent Heuken <me@vincentheuken.com>
2016-01-31 21:01:45 -0800

    finished the tests for get-action

--- src/frontend/uploads.cljs
+++ src/frontend/uploads.cljs
@@ -19,11 +19,20 @@
 
 (defn get-action [old-state new-state upload-id]
   "returns the action applied to the given upload-id.
-   Can return: :stopped, :started, :paused, :unpaused. :reconnected"
+   Can return: :stopped, :started, :paused, :resumed. :reconnected"
   (let [old-uploads (:uploads old-state)
         new-uploads (:uploads new-state)
         old-upload (old-uploads upload-id)
         new-upload (new-uploads upload-id)]
     (if (and (not (:stopped? new-upload))
              (:stopped? old-upload))
-      :started)))
+      :started
+      (if (and (:stopped? new-upload)
+               (not (:stopped? old-upload)))
+        :stopped
+        (if (and (:paused? new-upload)
+                 (not (:paused? old-upload)))
+          :paused
+          (if (and (not (:paused? new-upload))
+                   (:paused? old-upload))
+            :resumed))))))
--- test/frontend/uploads-test.cljs
+++ test/frontend/uploads-test.cljs
@@ -29,13 +29,42 @@
           new-state {:uploads {upload-id (uploads/start upload)}}]
       (is (nil? (uploads/get-action old-state new-state upload-id))))))
 
-#_(deftest get-action-stopped
+(deftest get-action-stopped
   (let [filename "foo.mp3"
         upload-id "test-id"
         upload (merge uploads/blank-upload {:filename filename
-                                            :id upload-id})
-        old-state {:uploads {}}
-        new-state {:uploads {upload-id
-                             (uploads/stop upload)}}]
-    (is (= :stopped (uploads/get-action old-state new-state upload-id)))))
+                                            :id upload-id})]
+    (let [old-state {:uploads {}}
+          new-state {:uploads {upload-id
+                               (uploads/stop upload)}}]
+      (is (= :stopped (uploads/get-action old-state new-state upload-id))))
+    (let [old-state {:uploads {upload-id (uploads/stop upload)}}
+          new-state {:uploads {upload-id (uploads/stop upload)}}]
+      (is (nil? (uploads/get-action old-state new-state upload-id))))))
 
+(deftest get-action-paused
+  (let [filename "foo.mp3"
+        upload-id "test-id"
+        upload (merge uploads/blank-upload {:filename filename
+                                            :id upload-id})]
+    (let [old-state {:uploads {upload-id (uploads/stop upload)}}
+          new-state {:uploads {upload-id
+                               (uploads/pause upload)}}]
+      (is (= :paused (uploads/get-action old-state new-state upload-id))))
+    (let [old-state {:uploads {upload-id (uploads/pause (uploads/stop upload))}}
+          new-state {:uploads {upload-id (uploads/pause (uploads/stop upload))}}]
+      (is (nil? (uploads/get-action old-state new-state upload-id))))))
+
+(deftest get-action-resumed
+  (let [filename "foo.mp3"
+        upload-id "test-id"
+        upload (merge uploads/blank-upload {:filename filename
+                                            :id upload-id})]
+    (let [old-state {:uploads {upload-id
+                               (uploads/pause (uploads/stop upload))}}
+          new-state {:uploads {upload-id
+                               (uploads/resume (uploads/pause (uploads/stop upload)))}}]
+      (is (= :resumed (uploads/get-action old-state new-state upload-id))))
+    (let [old-state {:uploads {upload-id (uploads/resume (uploads/stop upload))}}
+          new-state {:uploads {upload-id (uploads/resume (uploads/stop upload))}}]
+      (is (nil? (uploads/get-action old-state new-state upload-id))))))
