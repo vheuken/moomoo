2434e4763249906b61bbf61317477af98779a658
Vincent Heuken <me@vincentheuken.com>
2016-02-09 16:02:33 -0800

    fixed bug in uploads

--- src/frontend/uploads.cljs
+++ src/frontend/uploads.cljs
@@ -1,4 +1,5 @@
-(ns moomoo-frontend.uploads)
+(ns moomoo-frontend.uploads
+  (:require [clojure.set]))
 
 (defonce  blank-upload {:stopped? true
                         :paused?  false
@@ -36,3 +37,17 @@
           (if (and (not (:paused? new-upload))
                    (:paused? old-upload))
             :resumed))))))
+
+(defn handle-state-change [old-state new-state]
+  "Returns new state map according to changes between old-state and new-state"
+  ; uploads added
+  (let [new-upload-ids (clojure.set/difference (set (:room-uploads-order new-state))
+                                               (set (:room-uploads-order old-state)))]
+    (if (and (not (empty? new-upload-ids))
+             (< (count (:room-uploads-order new-state)) (:upload-slots new-state)))
+      (assoc new-state
+             :uploads
+             (assoc (:uploads new-state)
+                    (first new-upload-ids)
+                    (start ((:uploads new-state) (first new-upload-ids)))))
+      new-state)))
--- test/frontend/uploads-test.cljs
+++ test/frontend/uploads-test.cljs
@@ -76,3 +76,39 @@
         old-state {:uploads {upload-id upload}}
         new-state old-state]
     (is (nil? (uploads/get-action old-state new-state upload-id)))))
+
+(deftest uploads-watcher-upload-added
+ (let [upload-id "UPLOAD-ID"
+       filename "foo.mp3"
+       upload-slots 4
+       upload (merge uploads/blank-upload {:filename filename
+                                           :id upload-id})
+       old-room-uploads-order []
+       new-room-uploads-order [upload-id]
+       old-state {:uploads {}
+                  :upload-slots upload-slots
+                  :room-uploads-order old-room-uploads-order}
+       new-state {:uploads {upload-id upload}
+                  :upload-slots upload-slots
+                  :room-uploads-order new-room-uploads-order}]
+   (is (not (:stopped? ((:uploads (uploads/handle-state-change old-state new-state)) upload-id)))))
+ (let [upload-id-1 "foo"
+       filename-1 "foo.mp3"
+       upload-1 (uploads/start (merge uploads/blank-upload {:filename filename-1
+                                                            :upload-id upload-id-1}))
+       upload-id-2 "foo1"
+       filename-2 "foo1.mp3"
+       upload-2 (merge uploads/blank-upload {:filename filename-2
+                                             :upload-id upload-id-2})
+       upload-slots 1
+       old-uploads-order [upload-id-1]
+       new-uploads-order [upload-id-1 upload-id-2]
+       old-state {:uploads {upload-id-1 upload-1}
+                  :room-uploads-order old-uploads-order
+                  :upload-slots upload-slots}
+       new-state {:uploads {upload-id-1 upload-1 upload-id-2 upload-2}
+                  :room-uploads-order new-uploads-order
+                  :upload-slots upload-slots}
+       changed-state (uploads/handle-state-change old-state new-state)]
+    (println changed-state)
+    (is (:stopped? ((:uploads changed-state) upload-id-2)))))
