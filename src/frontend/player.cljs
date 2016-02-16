1afcc300a5e43002a8ff7a8c3e487d41fea4ff55
Vincent Heuken <me@vincentheuken.com>
2016-02-15 18:36:28 -0800

    finished player

---
+++ src/frontend/player.cljs
@@ -0,0 +1,26 @@
+(ns moomoo-frontend.player
+  (:require [moomoo-frontend.globals :as g]))
+
+(defn while-playing []
+  )
+
+(defn duration []
+  (if-let [current-sound (.getSoundById js/soundManager
+                                          (:current-sound-id @g/app-state))]
+    (.-duration current-sound)
+    0))
+
+(defn load-track! [sound-url sound-id on-load-callback]
+  (.createSound js/soundManager #js {:id sound-id
+                                     :url sound-url
+                                     :autoLoad true
+                                     :onload on-load-callback}))
+
+(defn play-track! [sound-id position on-finish-callback]
+  (swap! g/app-state assoc :current-sound-id sound-id)
+  (.play js/soundManager sound-id #js {:whileplaying while-playing
+                                       :onfinish on-finish-callback
+                                       :volume (:volume @g/app-state)
+                                       :from (if (> (duration) position)
+                                               position
+                                               (duration))}))
