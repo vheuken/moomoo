f2dbf49a80a806966178737d948c876489ae31f8
Vincent Heuken <me@vincentheuken.com>
2016-03-21 15:38:06 -0700

    bumped Clojurescript version

--- project.clj
+++ project.clj
@@ -2,8 +2,8 @@
   :description "Listen to music with friends."
   :url "https://github.com/vheuken/moomoo"
 
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/clojurescript "1.7.228"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/clojurescript "1.8.34"]
                  [org.omcljs/om "0.9.0"]
                  [com.cognitect/transit-cljs "0.8.225"]
                  [com.cemerick/piggieback "0.2.1"]
