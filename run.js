f7dcee7e49f9961c9ce3ff85e702f93ff277383c
Vincent Heuken <me@vincentheuken.com>
2016-09-11 18:58:42 -0700

    removed run.js

--- run.js
+++
@@ -1,8 +0,0 @@
-try {
-    require("source-map-support").install();
-} catch(err) {
-}
-require("./target/goog/bootstrap/nodejs.js");
-require("./target/moomoo.js");
-goog.require("moomoo.core");
-goog.require("cljs.nodejscli");
