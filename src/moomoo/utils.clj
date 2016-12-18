edfab7682c3f5cb12518cc13036221eeed6112b5
Vincent Heuken <me@vincentheuken.com>
2016-12-18 00:07:16 -0800

    added docstring to vector->hash-map

--- src/moomoo/utils.clj
+++ src/moomoo/utils.clj
@@ -3,6 +3,9 @@
 (defn uuid [] (str (java.util.UUID/randomUUID)))
 
 (defn vector->hash-map [v]
+  "Converts a vector to a hash-map like so: 
+   [\"a\" \"b\" :c 1] -> {:a \"b\", :c 1}.
+   Assumes an even number of elements."
   (apply hash-map (map-indexed #(if (even? %1)
                                   (keyword %2)
                                   %2)
