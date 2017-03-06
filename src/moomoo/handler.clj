b3d6ae71224857f83e6f8189d52f8d19b5ddd1b3
Vincent Heuken <me@vincentheuken.com>
2017-03-05 23:36:10 -0800

    fixed typo

--- src/moomoo/handler.clj
+++ src/moomoo/handler.clj
@@ -17,13 +17,12 @@
 (let [{:keys [ch-recv send-fn connected-uids
               ajax-post-fn ajax-get-or-ws-handshake-fn]}
       (sente/make-channel-socket! (get-sch-adapter) {})]
-()
   (def ring-ajax-post                ajax-post-fn)
   (def ring-ajax-get-or-ws-handshake ajax-get-or-ws-handshake-fn)
   (def ch-chsk                       ch-recv) ; ChannelSocket's receive channel
   (def chsk-send!                    send-fn) ; ChannelSocket's send API fn
   (def connected-uids                connected-uids) ; Watchable, read-only atom
-  )
+)
 
 (defroutes app-routes
   (GET "/" [] "<h1>Hello world!</h1>")
